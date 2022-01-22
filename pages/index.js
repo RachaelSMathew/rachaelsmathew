d9b65fcfab2cd2a303b4b8f954be84488c565e89
Rachael Mathew <rachaelmathew1@gmail.com>
2022-01-22 01:19:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Footer from './FooterComponent'
 import About from './AboutComponent'
 import Experiences from './ExperiencesComponent'
 import Skills from './SkillsComponent'
+import axios from "axios"
 
-
-export default function Home() {
+function Home({ data }) {
         return (
             <div className="container">
             <Head>
@@ -30,3 +30,18 @@ export default function Home() {
                 
         )
 }
+export async function getStaticProps() {
+   const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/todos/1`
+   );
+    const { data2 } = await axios.get(
+     'https://raw.githubusercontent.com/RachaelSMathew/rachaelsmathew/main/imageURL.json'
+    );
+
+   return {
+      props: {
+        data
+     },
+  };
+}
+export default Home;
